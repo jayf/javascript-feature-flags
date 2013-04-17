4e1b1278fa6d00c8364c413b5a57990ff7271416
Jay Fienberg <siteinfo@icite.net>
2013-04-16 22:40:51 -0700

    gh pages initial commit

---
+++ README.md
@@ -0,0 +1,105 @@
+javascript-feature-flags
+========================
+
+Get Feature Flags set in URL fragment, query string, browser cookies, and/or test domain name.
+
+##What are Feature Flags
+
+A *Feature Flag* is a setting to turn a feature on or off. This is often used to turn on experimental features in your code, e.g., for testing.
+
+###Example
+
+```html
+if ( ff.flag('crazy') ) {
+    doCrazyExperiment();
+} else {
+    doReallySafeStuff();
+}
+```
+
+##And This Respository Is...
+
+This is a Javascript function that reads feature flags in URL Fragments, query strings, browser cookies, and/or a test domain name. These are only "on" flags--they do not represent values or states beyond "on."
+
+The idea is: when a flag is absent, it is *off.* When a flag is present, it is *on.*
+
+The function then allows you to test for specific flags being present / on, using the above **ff.flag('your-flag-name')** syntax.
+
+This function has no dependencies on other JS libaries, and should work on all modern desktop browsers (and IE 9; it does **not** work on IE8 or earlier).
+
+Note that this library doesn't help you set flags per se--it just reads them.
+
+
+##Basic Usage
+
+```html
+<script src="feature-flags.js"></script>
+<script>
+    if ( ff.flag('your-flag-name') ) {
+        console.log('the your-flag-name flag is set!');
+    }
+</script>
+```
+
+##Advanced Usage
+
+Take a look at the feature-flags.js file. This file can be integrated into your existing Javascript library, and you may change the global variable name (which is ff), or attach this to an existing global, and override any of the following:
+
+```javascript
+(ff, {
+keyName: 'ff',
+testDomain: 'localhost',
+testDomainFlag: 'debug',
+methods: ['cookie', 'domain', 'hash', 'query'])
+```
+
+##Flag Syntax
+
+The flags that this function recognizes need to use a consistent name format. This is the **keyname** option, which defaults to **ff**.
+
+Note that comma separated values are always turned into separate flags
+
+Syntax examples (todo: document a bit more)
+
+###Query Strings
+
+All valid:
+
+````
+?ff=Kurt
+?ff=Bertold&ff=Kurt
+?ff=Elvin,Ringo
+?ff=Jimi,Eddie&ff=George,Sonny
+````
+
+###Fragments
+
+All valid:
+
+````
+#ff,Kurt
+#ff,Elvin,Ringo
+````
+
+###Cookies
+
+Set a cookie with the keyName (e.g., **ff**) and one value, or comma separated values
+
+
+###Test Domain
+
+By default localhost will set a debug flag. Via the Advance Usage examples above, you can change this to match your test domain name, and change the flag that is set.
+
+##Example
+
+See the index.html page for more examples.
+
+
+##License
+Copyright (c) 2013 Jay Fienberg
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ feature-flags.js
@@ -0,0 +1,143 @@
+/*! Javascript Feature Flags
+    Jay Fienberg, MIT License 2012
+    https://github.com/jayf/javascript-feature-flags
+
+*/
+
+/*
+    You can change this ff any variable name,
+    but change it again, to match at the bottom
+*/
+var ff = ff || {};
+
+(function(app, options) {
+    "use strict";
+
+    // defaults
+    app.methods = options && options.methods ||
+        [
+            'query',
+            'hash',
+            'cookie',
+            'domain'
+        ];
+
+    app.keyName = options && options.keyName ||'ff';
+    app.testDomain = options && options.testDomain || 'localhost';
+    app.testDomainFlag = options && options.testDomainFlag || 'debug';
+    app.loc = window.location;
+
+    app.flags = [];
+
+    // returns true if flag is set, false otherwise
+    // example: if ( ff.flag('Shuggie') ) { console.log('Otis!'); }
+    app.flag = function( f ) {
+        return false || app.flags.indexOf( f ) > -1;
+    };
+
+    // all methods for setting flags
+    app.setFlags = {
+
+        // flags stored in browser cookie named app.keyName
+        // cookie can be single value, or CSV, like: Brubeck,Coltrane,Monk
+        cookie: function() {
+            var neq = app.keyName + '=',
+                neql = neq.length,
+                value = null,
+                ca = document.cookie.split(';'),
+                cal = ca.length,
+                i;
+
+            for( i=0; i < cal; i++ ) {
+
+                var c = ca[i],
+                    cl = c.length;
+
+                while ( c.charAt(0) === ' ' ) {
+                    c = c.substring( 1, cl );
+                }
+
+                if ( c.indexOf(neq) === 0 ) {
+                    value = c.substring( neql, cl );
+                }
+            }
+
+            if ( value ) {
+                this._addFlag( value.split(',') );
+            }
+        },
+
+        // flag for page on a test domain
+        domain: function() {
+            if ( app.loc.hostname === app.testDomain ) {
+                this._addFlag(app.testDomainFlag);
+            }
+        },
+
+        // flag in URL fragment that starts with app.keyName
+        // fragment format is CSV after app.keyName: ff,Colins,Clinton,Hazel
+        hash: function() {
+            var h = app.loc.hash,
+                ki = h.indexOf( app.keyName );
+
+            if ( ki > -1 ) {
+                var s = h.substring( ki + app.keyName.length + 1 );
+                this._addFlag( s.split(',') );
+            }
+        },
+
+        // flag in URL query string, app.keyName is parameter
+        // both ?ff=Elvin&ff=Ringo and ?ff=Jones,Starr work
+        query: function() {
+            var s = app.loc.search,
+                a = s.substring(1).split('&'),
+                al = a.length,
+                i;
+
+            for ( i=0; i < al; i++ ) {
+                var p = a[i].split('=');
+
+                if ( p[0] === app.keyName && p[1] ) {
+                    this._addFlag( p[1].split(',') );
+                }
+            }
+        },
+
+        // private, adds each new flag to app.flags array
+        _addFlag: function(f) {
+            if ( typeof(f) === 'string' ) {
+                app.flags.push(f);
+            } else {
+                var i;
+                for ( i in f ) {
+                    app.flags.push( f[i] );
+                }
+            }
+        }
+    };
+
+    // call each desired method in app.setFlags
+    var m;
+    for ( m in app.methods ) {
+        app.setFlags[app.methods[m]]();
+    }
+
+})( ff );
+/*
+    If you change the ff variable name at top,
+    change it in the line above.
+
+    You also could attach this to an existing global,
+    just pass the function YO.ff
+
+    You can override any of the following:
+
+    (ff, {
+    keyName: 'ff',
+    testDomain: 'localhost',
+    testDomainFlag: 'debug',
+    methods: ['cookie', 'domain', 'hash', 'query'])
+}
+
+
+*/
